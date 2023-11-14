a4bd6377e5694a4a1c3152344cb8101dcb346cf9
t.vladimir <vovatatosyan2000@mail.ru>
2023-11-14 20:58:22 +0300

    Add injection to db in application layer

--- internal/adapters/app/api/api.go
+++ internal/adapters/app/api/api.go
@@ -3,16 +3,21 @@ package api
 import "github.com/m0rk0vka/hex/internal/ports"
 
 type Adapter struct {
+	db    ports.DbPort
 	arith ports.ArithmeticPort
 }
 
-func NewAdapter(arith ports.ArithmeticPort) *Adapter {
-	return &Adapter{arith: arith}
+func NewAdapter(db ports.DbPort, arith ports.ArithmeticPort) *Adapter {
+	return &Adapter{db: db, arith: arith}
 }
 
 func (apia Adapter) GetAddition(a, b int32) (int32, error) {
 	answer, err := apia.arith.Addition(a, b)
 	if err != nil {
+		return 0, err
+	}
+
+	if err := apia.db.AddToHistory(answer, "addition"); err != nil {
 		return 0, err
 	}
 
@@ -25,6 +30,10 @@ func (apia Adapter) Substruction(a, b int32) (int32, error) {
 		return 0, err
 	}
 
+	if err := apia.db.AddToHistory(answer, "substraction"); err != nil {
+		return 0, err
+	}
+
 	return answer, nil
 }
 
@@ -34,12 +43,20 @@ func (apia Adapter) Multitplication(a, b int32) (int32, error) {
 		return 0, err
 	}
 
+	if err := apia.db.AddToHistory(answer, "multiplication"); err != nil {
+		return 0, err
+	}
+
 	return answer, nil
 }
 
 func (apia Adapter) GetDivision(a, b int32) (int32, error) {
 	answer, err := apia.arith.Division(a, b)
 	if err != nil {
+		return 0, err
+	}
+
+	if err := apia.db.AddToHistory(answer, "division"); err != nil {
 		return 0, err
 	}
 
