b2645f7cdec84035583a6a839d1ec584d3528a37
t.vladimir <vovatatosyan2000@mail.ru>
2023-11-15 13:38:43 +0300

    Implement logic in cmd main file

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/m0rk0vka/hex/internal/adapters/app/api"
+	"github.com/m0rk0vka/hex/internal/adapters/core/arithmetic"
+	"github.com/m0rk0vka/hex/internal/adapters/framework/right/db"
+
+	gRPC "github.com/m0rk0vka/hex/internal/adapters/framework/left/grpc"
+	"github.com/m0rk0vka/hex/internal/ports"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	var err error
+	// ports
+	var core ports.ArithmeticPort
+	var appAdapter ports.APIPort
+	var dbaseAdapter ports.DbPort
+	var gRPCAdapter ports.GRPCPort
+
+	dbaseDriver := os.Getenv("DB_DRIVER")
+	dsourceName := os.Getenv("DS_NAME")
+
+	dbaseAdapter, err = db.NewAdapter(dbaseDriver, dsourceName)
+	if err != nil {
+		log.Fatalf("failed to initiate db connection: %v", err)
+	}
+	defer dbaseAdapter.CloseDbConnection()
+
+	core = arithmetic.NewAdapter()
+
+	appAdapter = api.NewAdapter(dbaseAdapter, core)
+
+	gRPCAdapter = gRPC.NewAdapter(appAdapter)
+	gRPCAdapter.Run()
 }
